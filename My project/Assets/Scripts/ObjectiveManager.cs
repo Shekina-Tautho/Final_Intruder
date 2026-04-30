f3fd561cad40c81b42a684ae476a1d5004330183
Shekina-Tautho <supagwapashikyna@gmail.com>
2026-04-30 12:09:12 +0800

    exit works on mouse and keyboard but not on vr controls

--- My project/Assets/Scripts/ObjectiveManager.cs
+++ My project/Assets/Scripts/ObjectiveManager.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using TMPro;
 
 public class ObjectiveManager : MonoBehaviour
 {
+    [Header("UI References")]
     public GameObject objectivePanel;
     public TextMeshProUGUI titleText;
     public TextMeshProUGUI bodyText;
@@ -13,13 +15,14 @@ public class ObjectiveManager : MonoBehaviour
     {
         if (isPanelActive)
         {
-            if (Input.anyKeyDown || Input.GetMouseButtonDown(0))
+            if (ShouldClosePanel())
             {
                 HidePanel();
             }
         }
     }
 
+    // ---------------- INTRO ----------------
     public void ShowIntroObjective()
     {
         isPanelActive = true;
@@ -28,15 +31,42 @@ public class ObjectiveManager : MonoBehaviour
         titleText.text = "Mission Start: Influenza Virus";
 
         bodyText.text =
-        "You are an Influenza Virus entering a human host.\n\n" +
-        "Primary Objective:\n- Infect target cells to increase infection count\n\n" +
-        "Threat:\n- Macrophages patrol and eliminate viruses\n\n" +
-        "Goal: Spread before immune system destroys you.";
+            "You are an Influenza Virus entering a human host.\n\n" +
+            "Primary Objective:\n" +
+            "- Infect target cells to increase infection count\n\n" +
+            "Threat:\n" +
+            "- Macrophages patrol and eliminate viruses\n\n" +
+            "Goal: Spread before immune system destroys you.";
     }
 
+    // ---------------- CLOSE ----------------
     void HidePanel()
     {
         isPanelActive = false;
         objectivePanel.SetActive(false);
+    }
+
+    // ---------------- INPUT ----------------
+    bool ShouldClosePanel()
+    {
+        // Keyboard
+        if (Keyboard.current != null && Keyboard.current.anyKey.wasPressedThisFrame)
+            return true;
+
+        // Mouse
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+            return true;
+
+        // VR placeholder
+        if (CheckVRInput())
+            return true;
+
+        return false;
+    }
+
+    bool CheckVRInput()
+    {
+        // Future XR integration goes here
+        return false;
     }
 }
